bbb49aebdd519b7e09d35156c7c72de367870f72
Julia <jrhuliafy@gmail.com>
2024-08-02 13:34:53 -0300

    :tada:Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// Elemento JSX/TSX => Função que retorna HTML
+
+import Home from "./pages/Home/Home"
+
+function App() {
+
+
+  return (
+    <>
+      <Home />
+      <h1>Cmponente react</h1>
+      </>
+    
+  )
+}
+
+export default App
