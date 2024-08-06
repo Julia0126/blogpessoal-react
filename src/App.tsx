9abb7ee4917d56d14fd4901bb99ed47cab2ffd0c
Julia <jrhuliafy@gmail.com>
2024-08-06 17:31:39 -0300

    fix: rotas

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,18 @@ import Login from './pages/Home/Home';
 function App() {
   return (
     <>
-    <BrowserRouter>
+      <BrowserRouter>
         <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
