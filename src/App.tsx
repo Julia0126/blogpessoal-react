bd6cf6a866ba3f6acca1e0381aafc1d803521fd9
Julia <jrhuliafy@gmail.com>
2024-08-09 01:34:51 -0300

    :sparkles:create: Models, contexts e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home/Home';
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer';
 
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Home/Home';
+import Home from './pages/Home/Home';
+import Cadastro from './pages/Cadastro/Cadastro';
+import Login from './pages/Login/Login';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
