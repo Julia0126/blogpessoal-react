fc0a38539311bb44af877c77c95cdc8f3fd3f4b1
Julia <jrhuliafy@gmail.com>
2024-08-15 21:14:57 -0300

    :bug: fix: retirar import react dos arquivos

--- src/components/Postagens/FormularioPostagem/FormularioPostagem.tsx
+++ src/components/Postagens/FormularioPostagem/FormularioPostagem.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { atualizar, buscar, cadastrar } from '../../../services/Service';
 import Postagem from '../../../models/Postagem';
 import Tema from '../../../models/Tema';
